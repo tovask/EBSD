a7d486a06311b2c710169e8013cf1986f0f8e2b6
Levente Kováts <tovask@users.noreply.github.com>
2020-02-17 18:36:07 +0100

    new data

--- filter_and_sum_misorientations.py
+++ filter_and_sum_misorientations.py
@@ -2,11 +2,11 @@
 #############
 # Adjustable parameters:
 #
-# The absolute threshold for interpret a value as a disorientation
+# The absolute threshold for interpret a value as a misorientation
 # (Beside 'absolute', it's can be compared 'relative' eg. to the average.)
 thres_abs = 0.7
 #
-# The number of values should be skipped before find a new disorientation
+# The number of values should be skipped before find a new misorientation
 skip_adjacent = 1
 #
 ############
@@ -19,6 +19,8 @@ all_y = {} # all the y values by directories
 out_result = {} # rendered values
 dirs = []
 
+#######
+"Parsing the files and find misorientations"
 files = sorted(glob.glob("**/*.txt")) # search for .txt files under directories
 for file in files:
     # print('Parsing file:',file)
@@ -48,7 +50,7 @@ for file in files:
         # print([float(sum([i for i in l if i>0]))/len([i for i in l if i>0]) for l in trans_values])
         # -----------
         # The important part:
-        disorient = []
+        misorient = []
         n = 0
         prev_n = -10
         for i in values:
@@ -58,29 +60,45 @@ for file in files:
             if prev_n + skip_adjacent >= n:
                 continue
             prev_n = n
-            disorient.append(i)
-        # print('\n'.join(map(lambda i: ', '.join(map(str,i)),disorient)))
+            misorient.append(i)
+        # print('\n'.join(map(lambda i: ', '.join(map(str,i)),misorient)))
         rel_distances = []
         prev_distance = 0
-        for row in disorient:
+        for row in misorient:
             rel_distances.append([row[0]-prev_distance, row[1]])
             prev_distance = row[0]
-        for row in rel_distances:
-            out = "; ".join( [str(round(row[0],9)), str(row[1])] )
-            # print(out)
+        # for row in rel_distances:
+        #     out = "; ".join( [str(round(row[0],9)), str(row[1])] )
+        #     print(out)
         file_results[file] = rel_distances # save the result
     # end of processing one file
 
 # print(file_results)
 
-x_file_re = re.compile(r'(.*_)x(\.txt)$')
-file_number_re = re.compile(r'.*_(\d+)_(?:x|y)\.txt$')
-dir_number_re = re.compile(r'(\d+)\/.*\.txt$')
 
-x_files = sorted(list(filter(lambda f: x_file_re.search(f), file_results.keys())))
+
+#########
+"Link corresponding x and y directions"
+
+x_file_re = re.compile(r'(.*_)x(\.txt)$')
+y_file_re = re.compile(r'(.*_)y(\.txt)$')
+file_number_re = re.compile(r'.*_(\d+)_(?:x|y)\.txt$')
+directory_re = re.compile(r'(.*)/[^/]+$')
+
+# error checking
+y_files = sorted(list(filter(lambda f: y_file_re.search(f), file_results.keys())))
+for y_file in y_files:
+    x_file = y_file_re.sub('\\1x\\2',y_file)
+    if not os.path.exists(x_file):
+        print("Error: missing corresponding y file:", x_file)
+        continue
+    if x_file not in file_results:
+        print("Error: missing corresponding y data:", x_file)
+        continue
+
+x_files = sorted(list(filter(lambda f: x_file_re.search(f), file_results.keys())), key = lambda s: int(file_number_re.search(s).group(1)))
 for x_file in x_files:
     y_file = x_file_re.sub('\\1y\\2',x_file)
-    # print(x_file, y_file, os.path.exists(y_file))
     if not os.path.exists(y_file):
         print("Error: missing corresponding y file:", y_file)
         continue
@@ -89,34 +107,35 @@ for x_file in x_files:
         continue
     # print(x_file)
     file_number = file_number_re.search(x_file).group(1)
-    dir_number = dir_number_re.search(x_file).group(1)
-    if not dir_number in dirs:
-        dirs.append(dir_number)
-    if not dir_number in out_result:
-        out_result[dir_number] = ""
-    if not dir_number in all_x:
-        all_x[dir_number] = []
-    if not dir_number in all_y:
-        all_y[dir_number] = []
-    out_result[dir_number] += file_number+'; ; ; ;\n'
+    print(int(file_number_re.search(x_file).group(1)))
+    directory = directory_re.search(x_file).group(1)
+    if not directory in dirs:
+        dirs.append(directory)
+    if not directory in out_result:
+        out_result[directory] = "Number; Misorientation x; Relative Distance x; Misorientation y; Relative Distance y\n"
+    if not directory in all_x:
+        all_x[directory] = []
+    if not directory in all_y:
+        all_y[directory] = []
+    out_result[directory] += file_number+'; ; ; ;\n'
     # place x and y results next to each other
     concated = list(itertools.zip_longest(file_results[x_file], file_results[y_file]))
     for row in concated:
         if row[0]:
             x_vals = list(map(lambda i: round(i, 9), row[0][::-1]))
-            all_x[dir_number].append(x_vals)
+            all_x[directory].append(x_vals)
         else:
             x_vals = ['','']
         if row[1]:
             y_vals = list(map(lambda i: round(i, 9), row[1][::-1]))
-            all_y[dir_number].append(y_vals)
+            all_y[directory].append(y_vals)
         else:
             y_vals = ['','']
-        out_result[dir_number] += "; ".join(map(str,['']+x_vals+y_vals)) + '\n'
+        out_result[directory] += "; ".join(map(str,['']+x_vals+y_vals)) + '\n'
 
 # write out the results
 for dir in dirs:
-    out_file = open('summary_'+dir+'.csv', 'w')
+    out_file = open(dir+'/'+'summary_'+dir+'.csv', 'w')
     out_file.write(out_result[dir])
     # transpose lists for caculate the average of each column
     all_x_trans = list(zip(*all_x[dir]))
